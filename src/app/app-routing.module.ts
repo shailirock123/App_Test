e57e598cf8a8c2d1421862e6592c96de02e576c0
Shailendra Pratap Singh <pratap.shailendra9@gmail.com>
2023-11-29 16:51:56 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'admin', loadChildren:()=>import('./modules/admin/admin.module').then(m =>m.AdminModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
